de9a14897d5b9aaff30e79adcb9583133190bfbe
Nikhil <nikhikul@amazon.com>
2023-04-10 20:39:56 +0530

    Git Push NextJS app - test 2

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,6 +1,6 @@
-//export default function handler(req, res) {
-//    const forwarded = req.headers['x-forwarded-for'];
-//    const ip = forwarded ? forwarded.split(/, /)[0].replace(/^.*:/, '') : req.socket.remoteAddress;
-//    const ipv4 = ip.includes(':') ? ip.split(':').reverse()[0] : ip;
-//    res.status(200).json({ ip: ipv4 });
-//  }
+export default function handler(req, res) {
+    const forwarded = req.headers['x-forwarded-for'];
+    const ip = forwarded ? forwarded.split(/, /)[0].replace(/^.*:/, '') : req.socket.remoteAddress;
+    const ipv4 = ip.includes(':') ? ip.split(':').reverse()[0] : ip;
+    res.status(200).json({ ip: ipv4 });
+  }
