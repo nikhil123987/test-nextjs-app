5c2ce24f7f6681391eb279f9cecc4327b0f0adb7
Nikhil <nikhikul@amazon.com>
2023-04-11 07:40:21 +0530

    Git Push NextJS app - test 3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
+  output: "standalone",
   eslint: { ignoreDuringBuilds: true },
   reactStrictMode: true,
   staticPageGenerationTimeout: 5000,
